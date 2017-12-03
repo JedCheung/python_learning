9647cb3b973b938e880fafa0550a836c7cb69f1b
JedCheung <zhang_yeda@163.com>
2017-12-03 17:34:20 +0800

    correct the constant name

--- basic/the_string.py
+++ basic/the_string.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-s1 = 72
-s2 = 85
-r = 100.0 * (s2-s1)/s1
+S1 = 72
+S2 = 85
+IMPROVEMENT = 100.0 * (S2-S1)/S1
 print('Name: %s Age: %s Gender: %s' % ("小明", 5, True))
-print('Score in the past two years: S1: %d, S2: %d' % (s1, s2))
-print('improvemt: %.1f %%' % r)
+print('Score in the past two years: S1: %d, S2: %d' % (S1, S2))
+print('improvemt: %.1f %%' % IMPROVEMENT)

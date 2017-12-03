3ff4045a6d80270422ea06989cf5ce13ceccac2a
JedCheung <zhang_yeda@163.com>
2017-12-03 17:31:22 +0800

    ok

--- basic/the_string.py
+++ basic/the_string.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-print('Name: %s Age: %s Gender: %s' % ("小明", 5, True))
 s1 = 72
 s2 = 85
-print('Score in the past two years: S1: %d, S2: %d' % (s1, s2))
 r = 100.0 * (s2-s1)/s1
+print('Name: %s Age: %s Gender: %s' % ("小明", 5, True))
+print('Score in the past two years: S1: %d, S2: %d' % (s1, s2))
 print('improvemt: %.1f %%' % r)

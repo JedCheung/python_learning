c5174fdea14bdd604453e3e1e6c81a4703902d98
JedCheung <zhang_yeda@163.com>
2018-01-09 01:10:38 +0800

    add fn_test

--- basic/fn_test.py
+++ basic/fn_test.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
 """ This is a python fn test demo. """
-INPUT = input('Input the num to transform:')
-print(hex(int(INPUT)))
+import math
+
+def quadratic(a, b, c):   
+    """this is a fn for cal the roots of ax^2 + bx + c = 0"""
+    if a == 0:
+        raise ('The input should be int or float')
+    if not isinstance(a, (int, float)):
+        raise TypeError('The input should be int or float')
+    if not isinstance(b, (int, float)):
+        raise TypeError('The input should be int or float')
+    if not isinstance(c, (int, float)):
+        raise TypeError('The input should be int or float')
+    r1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    r2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    return r1, r2
+
+
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+
+if quadratic(2, 3, 1) != (-0.5, -1.0):
+    print('测试失败')
+elif quadratic(1, 3, -4) != (1.0, -4.0):
+    print('测试失败')
+else:
+    print('测试成功')

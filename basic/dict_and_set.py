dc866b9d8a00ea716c1d42dd7d45e0fcb162ad3b
JedCheung <zhang_yeda@163.com>
2018-01-08 22:00:08 +0800

    add dict an set

--- basic/dict_and_set.py
+++ basic/dict_and_set.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
 """ This is a python dict and set test demo. """
+DICT_INSTANCE = {'zhang':25, 'zhou':24, 'zhao':28}
+print(DICT_INSTANCE)
+print(DICT_INSTANCE['zhang'])
+DICT_INSTANCE.pop('zhang')
+print(DICT_INSTANCE)
+
+SET_INSTANCE = set([1, 2, 3])
+print(SET_INSTANCE)
+#SET_INSTANCE.add(['qian', 'li'])
+#print(SET_INSTANCE) #TypeError: unhashable type: 'list'
+SET_INSTANCE.add('qian')
+print(SET_INSTANCE)
+
+TUPLE_INSTANCE = (4, 5, 6)
+SET_INSTANCE.add(TUPLE_INSTANCE) #指向都不变的tuple可以添加进去
+print(SET_INSTANCE)
+
+TUPLE_INSTANCE = (4, [5, 6])
+SET_INSTANCE.add(TUPLE_INSTANCE)  # 指向变的tuple不可以添加进去
+print(SET_INSTANCE)

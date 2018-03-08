aab6956b1aafc3911c7fbd33e732247084d3ff46
JedCheung <zhang_yeda@163.com>
2018-03-08 19:09:38 +0800

    add OPP

---
+++ basic/beast.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+class Timer(object):
+    def run(self):
+        print('This is time eater.')
+
+def run_twice(nothing):
+    nothing.run()
+    nothing.run()
+
+a = Animal()
+d = Dog()
+c = Cat()
+d = Timer()
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
+
+run_twice(d)
+run_twice(c)

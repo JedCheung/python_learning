327aff40c0cf4b4486e1206f8f924e9fe37933a9
JedCheung <zhang_yeda@163.com>
2018-03-08 20:20:58 +0800

    add if.py

--- basic/the_string.py
+++ basic/the_string.py
@@ -6,3 +6,11 @@ IMPROVEMENT = 100.0 * (S2-S1)/S1
 print('Name: %s Age: %s Gender: %s' % ("小明", 5, True))
 print('Score in the past two years: S1: %d, S2: %d' % (S1, S2))
 print('improvemt: %.1f %%' % IMPROVEMENT)
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0], L[1][1], L[2][2])
+print('Done!')
